2e70656aefc5ff0c3dd812c1d1c90e38f01bd233
Rishi Raj Prasad <rr8382658+github@gmail.com>
2025-11-28 23:34:30 +0530

    Update [[catchall]].js

--- api/[[catchall]].js
+++ api/[[catchall]].js
@@ -1,6 +1,6 @@
 export async function onRequest(context) {
   // FIXED: Pointing to the correct worker name 'cipet-portal'
-  const WORKER_URL = "https://vite-react-template.rishiforrdp6055.workers.dev";
+  const WORKER_URL = "cipet-portal.rishiforrdp6055.workers.dev";
   
   const url = new URL(context.request.url);
   // Ensure the path passed to the worker is correct
