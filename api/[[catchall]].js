71042bbe4c0c71bceccfab59d66b43cd3810c4e3
Rishi Raj Prasad <rr8382658+github@gmail.com>
2025-11-28 22:30:39 +0530

    Update [[catchall]].js

--- api/[[catchall]].js
+++ api/[[catchall]].js
@@ -1,11 +1,9 @@
 export async function onRequest(context) {
-  // Replace with your actual Worker URL
-  const WORKER_URL = "https://vite-react-template.rishiforrdp6055.workers.dev";
+  // REPLACE WITH YOUR ACTUAL WORKER URL (Found in Cloudflare Dashboard -> Workers)
+  const WORKER_URL ="https://vite-react-template.rishiforrdp6055.workers.dev";
   
   const url = new URL(context.request.url);
   const workerUrl = new URL(url.pathname, WORKER_URL);
-  
-  // Forward request to Worker
   const newRequest = new Request(workerUrl, context.request);
   return fetch(newRequest);
 }
