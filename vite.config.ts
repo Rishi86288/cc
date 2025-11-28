7c26d35821c0508b5c5927aaf9ae86c8f16d217f
Rishi Raj Prasad <rr8382658+github@gmail.com>
2025-11-29 01:07:00 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/", 
@@ -13,14 +12,6 @@ export default defineConfig({
   },
   build: {
     outDir: "dist",
-    sourcemap: true,
-    // CRITICAL FIX: Externalize Firebase modules so Rollup doesn't try to bundle the massive SDK
-    rollupOptions: {
-        external: [
-            /^firebase\/.*/, // Exclude all firebase subpackages
-            'firebase/app',
-            'firebase/auth'
-        ]
-    }
+    sourcemap: true
   }
 });
